af79e7c679d4074e7f7b2b7355d926560fbc3e00
harchebnikovaay <harchebnikova99@mail.ru>
2021-02-12 14:42:54 +0700

    Update

--- demo/models.py
+++ demo/models.py
@@ -5,6 +5,6 @@ class Book(models.Model):
     title=models.CharField(max_length=40, blank=False, unique=True)
     description=models.TextField(max_length=500, blank=True)
     price=models.DecimalField(default=0, max_digits=6, decimal_places=2)
-    published=models.DateTimeField
-    is_published=models.BooleanField
+    published=models.DateTimeField()
+    is_published=models.BooleanField()
     cover=models.ImageField(upload_to='covers/')

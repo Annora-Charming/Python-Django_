aa6937a371326a09ceae1c1d4d114dea11a8fddb
harchebnikovaay <harchebnikova99@mail.ru>
2021-02-12 14:38:11 +0700

    Update

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title=models.CharField(max_length=40)
+    title=models.CharField(max_length=40, blank=False, unique=True)
+    description=models.TextField(max_length=500, blank=True)
+    price=models.DecimalField(default=0, max_digits=6, decimal_places=2)
+    published=models.DateTimeField
+    is_published=models.BooleanField
+    cover=models.ImageField(upload_to='covers/')
